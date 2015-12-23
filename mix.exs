5c35b636e64d1b585ca3b8d2fc398cf322f26eaf
Michael Truog <mjtruog@gmail.com>
2015-12-23 13:18:59 -0800

    Version 1.5.1.

---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule Supool.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :supool,
+     version: "1.5.1",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp description do
+    "Erlang Process Pool as a Supervisor"
+  end
+
+  defp package do
+    [files: ~w(src doc test rebar.config README.markdown),
+     maintainers: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"GitHub" => "https://github.com/okeuday/supool"}]
+   end
+end
