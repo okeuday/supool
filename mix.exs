7b2e412d7398567ee4728fbe4a284df6a6452e59
Michael Truog <mjtruog@protonmail.com>
2018-09-15 17:35:16 -0700

    Version 1.7.4.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,23 @@ defmodule Supool.Mixfile do
 
   def project do
     [app: :supool,
-     version: "1.7.3",
+     version: "1.7.4",
+     language: :erlang,
+     erlc_options: [
+       :debug_info,
+       :warnings_as_errors,
+       :strict_validation,
+       :warn_bif_clash,
+       :warn_deprecated_function,
+       :warn_export_all,
+       :warn_export_vars,
+       :warn_exported_vars,
+       :warn_obsolete_guard,
+       :warn_shadow_vars,
+       :warn_unused_import,
+       :warn_unused_function,
+       :warn_unused_record,
+       :warn_unused_vars],
      description: description(),
      package: package(),
      deps: deps()]
