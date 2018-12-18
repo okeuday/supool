d7aa39a27a641e9e966fc753c2f57c21b0f2a5fe
Michael Truog <mjtruog@protonmail.com>
2018-12-17 17:53:32 -0800

    Version 1.7.5.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Supool.Mixfile do
 
   def project do
     [app: :supool,
-     version: "1.7.4",
+     version: "1.7.5",
      language: :erlang,
      erlc_options: [
        :debug_info,
