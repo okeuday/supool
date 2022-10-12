b9a72e282dfa97b43a21a419a0fbcdb71aecb5f7
Michael Truog <mjtruog@protonmail.com>
2022-10-11 19:57:10 -0700

    Version 2.0.5.

--- mix.exs
+++ mix.exs
@@ -6,9 +6,10 @@ defmodule Supool.Mixfile do
 
   def project do
     [app: :supool,
-     version: "2.0.4",
+     version: "2.0.5",
      language: :erlang,
      erlc_options: [
+       :deterministic,
        :debug_info,
        :warn_export_vars,
        :warn_unused_import,
